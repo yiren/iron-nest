7b1b8dab47be6e33764bae74dd58e14b3cf429f5
yiren <yiren.goo@gmail.com>
2018-11-10 14:05:00 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,35 @@
 
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, FastifyAdapter } from '@nestjs/core';
 import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
 
-  const userApiOptions = new DocumentBuilder()
-            .setTitle('User API Doc')
-            .setDescription('User API Info')
-            .setVersion('1.0')
-            .addBearerAuth()
-            .addTag('users') // match tags in controllers
-            .build();
+  // const userApiOptions = new DocumentBuilder()
+  //           .setTitle('User API Doc')
+  //           .setDescription('User API Info')
+  //           .setVersion('1.0')
+  //           .addBearerAuth()
+  //           .addTag('users') // match tags in controllers
+  //           .build();
 
-  const userApiDocument= SwaggerModule.createDocument(app, userApiOptions, {include: [UserModule]});
-  SwaggerModule.setup('v1/api/user', app, userApiDocument);
+  // const userApiDocument= SwaggerModule.createDocument(app, userApiOptions, {include: [UserModule]});
+  // SwaggerModule.setup('v1/api/user', app, userApiDocument);
 
-  const authApiOptions = new DocumentBuilder()
-            .setTitle('Auth API Doc')
-            .setDescription('Auth API Info')
-            .setVersion('1.0')
-            .addBearerAuth()
-            .addTag('auth') // match tags in controllers
-            .build();
-  const authApiDocument= SwaggerModule.createDocument(app, authApiOptions, {include: [AuthModule]});
-  SwaggerModule.setup('v1/api/auth', app, authApiDocument);
+  // const authApiOptions = new DocumentBuilder()
+  //           .setTitle('Auth API Doc')
+  //           .setDescription('Auth API Info')
+  //           .setVersion('1.0')
+  //           .addBearerAuth()
+  //           .addTag('auth') // match tags in controllers
+  //           .build();
+  // const authApiDocument= SwaggerModule.createDocument(app, authApiOptions, {include: [AuthModule]});
+  // SwaggerModule.setup('v1/api/auth', app, authApiDocument);
 
   await app.listen(3000);
   
